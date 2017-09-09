4ee28090cd114d3bb945ef9f79c0efcada483e05
Lenka-Stejskal <lenka.stejskal.15@ucl.ac.uk>
2017-09-09 14:49:07 +0100

    Create Remove_Hydrogens_from_pdb.py

---
+++ Remove_Hydrogens_from_pdb.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2017 Lenka Stejskal
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+# Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+# Change the name of residues as specified in a control file 
+
+__author__ = 'Lenka Stejskal'
+
+import fileinput
+import sys
+import argparse
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='Remove H records from a PDB')
+    parser.add_argument('infile', help='input file (PDB format)')
+    parser.add_argument('outfile', help='output file (PDB format)')
+    args = parser.parse_args()
+
+    with open(args.infile, "r") as f, open(args.outfile, "w") as of:
+        for line in f:
+            if line[0:3] == "END":
+                of.write(line)
+            elif line[0:3] == 'TER':
+                of.write(line)
+            temp = line.split()
+            if len(temp) > 2:
+                    if temp[-2] != 'H' and temp[-1] !='H':
+                        of.write(line)
+            else:
+                continue
+if __name__ == "__main__":
+    main(sys.argv)
